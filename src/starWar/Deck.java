80489e69585a9e57846268cdb55bd9777994d0bb
Jean <mizero2@gmail.com>
2021-05-11 10:05:59 -0600

    Project set up

---
+++ src/starWar/Deck.java
@@ -0,0 +1,5 @@
+package starWar;
+
+public class Deck {
+
+}
---
+++ src/starWar/Player.java
@@ -0,0 +1,10 @@
+package starWar;
+
+public class Player {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
